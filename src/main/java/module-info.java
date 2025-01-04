d3ed2bdaf5b612485ed4535581971656c5525bab
Joelgs13 <050213jgonzalez@gmail.com>
2025-01-04 10:55:18 +0100

    commit inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module joel.dein.ejercicio3_jasper {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens joel.dein.ejercicio3_jasper to javafx.fxml;
+    exports joel.dein.ejercicio3_jasper;
+}
